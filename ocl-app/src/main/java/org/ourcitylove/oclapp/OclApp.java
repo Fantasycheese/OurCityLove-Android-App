fa3ad20bacc2a1b893b461245f8a89ea633d42e8
Joshua Lin <joshua_lin@dcms.nctu.edu.tw>
2016-05-24 17:34:55 +0800

    add some core librarys

--- ocl-app/src/main/java/org/ourcitylove/oclapp/OclApp.java
+++ ocl-app/src/main/java/org/ourcitylove/oclapp/OclApp.java
@@ -1,7 +1,10 @@
 package org.ourcitylove.oclapp;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.karumi.dexter.Dexter;
@@ -9,15 +12,19 @@ import com.karumi.dexter.Dexter;
 import org.acra.ACRA;
 
 public class OclApp extends Application {
-
+    public static final String TAG = OclApp.class.getSimpleName();
     protected static FirebaseAnalytics mFirebaseAnalytics;
+    public static SharedPreferences pref;
+    public static Context ctx;
 
     @Override
     public void onCreate() {
         super.onCreate();
         ACRA.init(this);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         Dexter.initialize(this);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        ctx = this;
     }
 
     public void trackScreen(String name) {
