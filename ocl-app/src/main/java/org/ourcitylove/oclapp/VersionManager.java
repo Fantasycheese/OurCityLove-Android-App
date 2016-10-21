21424015137d3c7d154ffb9bec84b4bea874c2fc
Joshua Lin <joshua_lin@dcms.nctu.edu.tw>
2016-10-21 15:14:05 +0800

    Version Manager singleton and builder pattern

--- ocl-app/src/main/java/org/ourcitylove/oclapp/VersionManager.java
+++ ocl-app/src/main/java/org/ourcitylove/oclapp/VersionManager.java
@@ -10,41 +10,40 @@ import com.github.javiersantos.appupdater.AppUpdater;
  */
 
 public class VersionManager {
-    private static VersionManager vm;
-    private static AppUpdater apu;
-    public static Activity act;
-    private static String Title, Content, Update, Dismiss, NotShow = "";
 
-    public static VersionManager init(Activity ac){
-        vm = null == vm?new VersionManager():vm;
-        vm.apu = new AppUpdater(ac)
+    private AppUpdater apu;
+    private Activity act;
+    private String Title, Content, Update, Dismiss, NotShow = "";
+
+    public VersionManager init(Activity ac){
+        apu = new AppUpdater(ac)
                 .showAppUpdated(false);
-        vm.act = ac;
-        vm.setTitle(R.string.TitleOnUpdateAvailable);
-        vm.setContent(act.getString(R.string.ContentOnUpdateAvailable, act.getString(R.string.app_name)));
-        vm.setUpdateButton(R.string.UpdateGo);
-        vm.setDismissButton(R.string.UpdateNextTime);
-        vm.setNotShowButton(R.string.DoNotShowAgain);
-        return vm;
+        act = ac;
+        setTitle(R.string.TitleOnUpdateAvailable);
+        setContent(act.getString(R.string.ContentOnUpdateAvailable, act.getString(R.string.app_name)));
+        setUpdateButton(R.string.UpdateGo);
+        setDismissButton(R.string.UpdateNextTime);
+        setNotShowButton(R.string.DoNotShowAgain);
+        return this;
     }
 
-    public static VersionManager setTitle(@StringRes int Res){vm.Title = vm.act.getString(Res);return vm;}
-    public static VersionManager setTitle(String Res){vm.Title = Res;return vm;}
+    public VersionManager setTitle(@StringRes int Res){Title = act.getString(Res);return this;}
+    public VersionManager setTitle(String Res){Title = Res;return this;}
 
-    public static VersionManager setContent(@StringRes int Res){vm.Content = vm.act.getString(Res);return vm;}
-    public static VersionManager setContent(String Res){vm.Content = Res;return vm;}
+    public VersionManager setContent(@StringRes int Res){Content = act.getString(Res);return this;}
+    public VersionManager setContent(String Res){Content = Res;return this;}
 
-    public static VersionManager setUpdateButton(@StringRes int Res){vm.Update = vm.act.getString(Res);return vm;}
-    public static VersionManager setUpdateButton(String Res){vm.Update = Res;return vm;}
+    public VersionManager setUpdateButton(@StringRes int Res){Update = act.getString(Res);return this;}
+    public VersionManager setUpdateButton(String Res){Update = Res;return this;}
 
-    public static VersionManager setDismissButton(@StringRes int Res){vm.Dismiss = vm.act.getString(Res);return vm;}
-    public static VersionManager setDismissButton(String Res){vm.Dismiss = Res;return vm;}
+    public VersionManager setDismissButton(@StringRes int Res){Dismiss = act.getString(Res);return this;}
+    public VersionManager setDismissButton(String Res){Dismiss = Res;return this;}
 
-    public static VersionManager setNotShowButton(@StringRes int Res){vm.NotShow = vm.act.getString(Res);return vm;}
-    public static VersionManager setNotShowButton(String Res){vm.NotShow = Res;return vm;}
+    public VersionManager setNotShowButton(@StringRes int Res){NotShow = act.getString(Res);return this;}
+    public VersionManager setNotShowButton(String Res){NotShow = Res;return this;}
 
-    public static void start() {
-        vm.apu.setTitleOnUpdateAvailable(Title)
+    public void start() {
+        this.apu.setTitleOnUpdateAvailable(Title)
            .setContentOnUpdateAvailable(Content)
            .setButtonUpdate(Update)
            .setButtonDismiss(Dismiss)
